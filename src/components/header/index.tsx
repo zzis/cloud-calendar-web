9bfd50a93ad02a2609d998a285ab8e457ef61fb7
zhaizhe <zhaizhe@didiglobal.com>
2019-07-18 00:09:29 +0800

    add style

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
+import style from './index.less';
+import logo from '@/assets/facebook_cover_photo_1.png';
+
 interface IHeaderState {}
 interface IHeaderProps {}
 
 export default class Header extends React.Component<IHeaderProps, IHeaderState> {
   public render() {
     return (
-      <div>Header</div>
+      <header className={style.header}>
+        {/* <img src={logo} alt='cloud calendar' className={style.logo}/> */}
+        <span className={style.logoText}>Cloud Calendar</span>
+      </header>
     );
   }
 }
