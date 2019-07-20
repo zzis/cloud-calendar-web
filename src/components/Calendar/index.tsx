4a19293bd431f9ad9d468ef0af8f935439d6809b
zhaizhe <zhaizhe@didiglobal.com>
2019-07-21 00:33:13 +0800

    add lodash decorator, add create schedule listener

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -5,6 +5,7 @@ import 'tui-date-picker/dist/tui-date-picker.css';
 import 'tui-time-picker/dist/tui-time-picker.css';
 
 import { Button } from 'antd';
+import { Bind } from 'lodash-decorators';
 
 import style from './index.less';
 
@@ -19,6 +20,11 @@ export default class Calendar extends React.Component<ICalendarProps, ICalendarS
     this.calendarInstance = this.calendarRef.current.getInstance();
   }
 
+  @Bind
+  public onBeforeCreateSchedule(event) {
+    // console.log('create item', event);
+  }
+
   public render() {
     return <div className={style.calendarContainer}>
       <TuiCalendar
@@ -26,6 +32,7 @@ export default class Calendar extends React.Component<ICalendarProps, ICalendarS
         height={500}
         useCreationPopup
         ref={this.calendarRef}
+        onBeforeCreateSchedule={this.onBeforeCreateSchedule}
       />
     </div>;
   }
